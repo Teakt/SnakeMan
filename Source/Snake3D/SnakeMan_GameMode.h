ececc79875d83bcac07905328dd67985fac69714
Teak <teakguillaume@gmail.com>
2022-03-02 17:16:33 +0100

    GameMode added for snakeMan

---
+++ Source/Snake3D/SnakeMan_GameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "SnakeMan_GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SNAKE3D_API ASnakeMan_GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
